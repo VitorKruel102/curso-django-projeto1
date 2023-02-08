a75dc21601a25f5f0f0dc47d18878368c4403ea9
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-02-07 22:16:01 -0300

    Adicionando aplicação no settings e criando pasta templates.

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home(request):
     """Website homepage."""
-    return HttpResponse('HOME')
+    template_name = 'recipes/home.html'
+    context = {'name': 'Vitor'}
+    return render(request, template_name, context)
 
 
 def contact(request):
     """Contact page."""
-    return HttpResponse('Contact')
+    template_name = 'recipes/contact.html'
+    return render(request, template_name)
 
 
 def about(request):
